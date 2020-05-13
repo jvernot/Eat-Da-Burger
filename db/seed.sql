76d5e630203f338674904ab2e70d0a8b56c1ad91
Jason Vernot <jason.vernot@gmail.com>
2020-05-12 20:52:09 -0400

    created initial files including server.js, package.json, lock file, git ignore, and db folder. Created burgers_db and seeds file.

---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('The Baconator');
+
+INSERT INTO burgers (burger_name) VALUES ('Buffalo Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('Beach Burger');
